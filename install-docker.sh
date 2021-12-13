abeb47f713c81fcff3216028dab78e14f44b161e
kissyouhunter <63334206+kissyouhunter@users.noreply.github.com>
2021-12-13 17:59:49 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -12,6 +12,6 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 docker -v
-curl -L "https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose -v
