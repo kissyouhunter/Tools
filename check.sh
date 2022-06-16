633a2e4440d26fb2eb48ec41b1cf319bcafb15aa
kissyouhunter <kissyouhunter@hotmail.com>
2022-06-16 09:43:25 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -3,10 +3,8 @@
 # check for whiptail
 
 if [ "$(command -v whiptail)" ]; then
-    bash
     bash <(curl -s -S -L https://raw.githubusercontent.com/kissyouhunter/Tools/main/menu.sh)
 else
-    bash
     bash <(curl -s -S -L https://raw.githubusercontent.com/kissyouhunter/Tools/main/kiss.sh)
 fi
 
