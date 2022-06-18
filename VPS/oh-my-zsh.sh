7f50514697dabe1d65bd7e8a06dfd06b292d3d3c
kissyouhunter <kissyouhunter@hotmail.com>
2022-06-18 16:01:06 +0800

    Update oh-my-zsh.sh

--- VPS/oh-my-zsh.sh
+++ VPS/oh-my-zsh.sh
@@ -9,7 +9,7 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-curl -Lo ~/.zshrc
+curl -Lo ~/.zshrc https://raw.githubusercontent.com/kissyouhunter/Tools/main/VPS/zshrc
 
 echo "please logout and log back in"
 
