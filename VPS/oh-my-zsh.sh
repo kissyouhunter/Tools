f96d5bd1def4f42300054a6c4658d6159c38fc1e
kissyouhunter <kissyouhunter@hotmail.com>
2022-06-18 16:19:27 +0800

    Update oh-my-zsh.sh

--- VPS/oh-my-zsh.sh
+++ VPS/oh-my-zsh.sh
@@ -5,8 +5,9 @@
 sudo apt update
 sudo apt install -y git wget curl nano zsh
 
-chsh -s $(which zsh)
+#chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 curl -Lo ~/.zshrc https://raw.githubusercontent.com/kissyouhunter/Tools/main/VPS/zshrc
