181b78643738ba2edc8f8f4682200691d87a0abf
kissyouhunter <kissyouhunter@hotmail.com>
2022-08-16 04:05:10 +0800

    Update upload.sh

--- rclone/upload.sh
+++ rclone/upload.sh
@@ -149,7 +149,7 @@ Remote Path: ${REMOTE_PATH}
             echo -e "$(DATE_TIME) ${ERROR} Upload failed! Retry ${RETRY}/${RETRY_NUM} ..."
             echo
         )
-        rclone -v moveto -P "${LOCAL_PATH}" "${REMOTE_PATH}"
+        rclone move -v "${LOCAL_PATH}" "${REMOTE_PATH}"
         TG_MSG1="[$(date '+%Y-%m-%d %H:%M:%S')] ${LOCAL_PATH} 上传到网盘 $NAME 的 ${REMOTE_PATH} 目录已完成。"
         RCLONE_EXIT_CODE=$?
         if [ ${RCLONE_EXIT_CODE} -eq 0 ]; then
