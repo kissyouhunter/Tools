264461b96c4c705dfd16ef8338494cdfab1e5c43
kissyouhunter <kissyouhunter@hotmail.com>
2022-10-26 18:10:03 +0800

    Create checkip.sh

---
+++ VPS/checkip.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#author kissyouhunter
+
+TG_TOKEN=""  #tg机器人的token
+TG_ID="" #tg的user ID
+
+function tg_push_message() {
+    TOKEN=${TG_TOKEN}	#TG机器人token
+    chat_ID=${TG_ID}		#用户ID或频道、群ID
+    message_text="${TG_MSG}"		#要发送的信息
+    MODE="HTML"		#解析模式，可选HTML或Markdown
+    URL="https://api.telegram.org/bot${TOKEN}/sendMessage"		#api接口
+    curl -s -o /dev/null -X POST $URL -d chat_id=${chat_ID} -d parse_mode=${MODE} -d text="${message_text}" --max-time 10
+}
+
+function check_ip() {
+	IP=$(curl -s ipv4.ip.sb)
+	NAME=$(cat /etc/hostname)
+	echo "${NAME}的IP地址为：${IP}"
+	TG_MSG="${NAME}的IP地址为：${IP}"
+	tg_push_message
+}
+
+check_ip
+
+exit 0
