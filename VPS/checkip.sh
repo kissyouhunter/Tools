e91956f76c65a34a3b3e92202bc6930e5b9fa73f
kissyouhunter <kissyouhunter@hotmail.com>
2022-10-26 18:50:43 +0800

    Update checkip.sh

--- VPS/checkip.sh
+++ VPS/checkip.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #author kissyouhunter
+### BEGIN INIT INFO
+# Provides:          kissyouhunter
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: self define auto start
+# Description:       self define auto start
+### END INIT INFO
 
 TG_TOKEN=""  #tg机器人的token
 TG_ID="" #tg的user ID
