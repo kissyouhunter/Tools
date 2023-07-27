709894de613781bb0639e3befc5bfec56988801e
kissyouhunter <kissyouhunter@hotmail.com>
2023-07-27 21:08:23 +0800

    Create netdata.sh

---
+++ VPS/netdata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 匹配物理网卡 
+netcard=$(cat /proc/net/dev | grep -E 'eth[0-9]+|wlan[0-9]+|enp[0-9]+|ens[0-9]+|eno[0-9]+|wl[0-9 ]+' | awk -F: '{print $1}' | tr -d ' ')
+
+# 获取流量信息并转换单位
+in=$(cat /proc/net/dev | grep $netcard | awk '{printf "%f\n", $2/1024/1024/1024}')
+out=$(cat /proc/net/dev | grep $netcard | awk '{printf "%f\n", $10/1024/1024/1024}')
+
+# 输出结果 
+echo "入站流量: $in GB"
+echo "出站流量: $out GB"
+
+exit 0
