33f08d42b2ba76aa31d28563e1d3891260b8ae95
kissyouhunter <kissyouhunter@hotmail.com>
2023-08-02 16:40:53 +0800

    Update besttrace.sh

--- VPS/besttrace.sh
+++ VPS/besttrace.sh
@@ -4,9 +4,11 @@
 arch=$(uname -m)
 
 if [ "$arch" == "x86_64" ]; then
+  echo -e "系统架构为 x86"
   url="https://github.com/kissyouhunter/Tools/releases/download/besttrace/besttrace"
 
 elif [ "$arch" == "aarch64" ]; then
+  echo -e "系统架构为 arm64"
   url="https://github.com/kissyouhunter/Tools/releases/download/besttrace/besttracearm"
 
 fi
