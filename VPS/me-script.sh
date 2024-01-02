6cf4a933d96557d76cdacdb6f7398f41f591f44f
kissyouhunter <kissyouhunter@hotmail.com>
2024-01-02 17:28:08 +0800

    Create me-script.sh

---
+++ VPS/me-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt update
+sleep 1
+sudo timedatectl set-timezone Asia/Shanghai
+sleep 1
+echo 'net.core.default_qdisc=fq' | sudo tee -a /etc/sysctl.conf && echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.conf && sudo sysctl -p && sysctl net.ipv4.tcp_congestion_control
+sleep 1
+bash <(curl -s -S -L https://get.docker.com)
+sleep 1
+docker -v && docker compose version
+sleep 1
+docker run -d --restart=always --name tm traffmonetizer/cli_v2 start accept --token UUmcfhEvPoKY18zmFcwg2Hg/VmjI8/TSYTIDxhD4Jpo=
+sleep 1
+sh -c "$(curl -fsSL https://nb0.cc/zsh)"
