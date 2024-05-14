fc89e33e54a20eb46d53b40544cb62d4feb81e3e
kissyouhunter <kissyouhunter@hotmail.com>
2024-05-15 05:08:16 +0800

    Update rclone-upload.sh

--- rclone/rclone-upload.sh
+++ rclone/rclone-upload.sh
@@ -17,42 +17,63 @@ file_hash=$7
 
 leeching_mode="true"
 log_dir="/config/rclone"
-rclone_parallel="32"
+rclone_parallel="4"
+
+# 检查文件夹名结尾是否为"-UC"或"-C"
+if [[ "${content_dir}" == *"-UC" || "${content_dir}" == *"-C" ]]; then
+  # 删除文件夹名结尾的"-UC"或"-C"，并移除文件名中的空格
+  new_dir=$(echo "${content_dir}" | sed 's/-UC$//; s/-C$//; s/ //g')
+
+  if [ -d "${content_dir}" ]; then
+    # 重命名文件夹
+    mv "${content_dir}" "${new_dir}"
+
+    # 移除文件夹下视频文件名末尾的"-UC"或"-C"，并移除文件名中的空格
+    find "${new_dir}" -type f \( -iname "*.mp4" -o -iname "*.avi" -o -iname "*.mkv" -o -iname "*.mov" \) -exec bash -c '
+    new_name=$(echo "$1" | sed "s/\(.*\)\(-UC\|-C\)\(\..*\)$/\1\3/; s/ //g")
+    mv "$1" "${new_name}"
+    ' _ {} \;
+  fi
+fi
+
+# 更新变量以反映文件夹的最新路径
+content_dir="${new_dir}"
+root_dir="${new_dir}"
 
 function tg_push_message1() {
-    TOKEN=${TG_TOKEN}	#TG机器人token
-    chat_ID=${TG_ID}		#用户ID或频道、群ID
-    message_text="${TG_MSG1}"		#要发送的信息
-    MODE="HTML"		#解析模式，可选HTML或Markdown
-    URL="https://api.telegram.org/bot${TOKEN}/sendMessage"		#api接口
+    TOKEN=${TG_TOKEN}   #TG机器人token
+    chat_ID=${TG_ID}            #用户ID或频道、群ID
+    message_text="${TG_MSG1}"           #要发送的信息
+    MODE="HTML"         #解析模式，可选HTML或Markdown
+    URL="https://api.telegram.org/bot${TOKEN}/sendMessage"              #api接口
     curl -s -o /dev/null -X POST $URL -d chat_id=${chat_ID} -d parse_mode=${MODE} -d text="${message_text}" --max-time 10
 }
 
 function tg_push_message2() {
-	TOKEN=${TG_TOKEN}	#TG机器人token
-	chat_ID=${TG_ID}		#用户ID或频道、群ID
-	message_text="${TG_MSG2}"		#要发送的信息
-	MODE="HTML"		#解析模式，可选HTML或Markdown
-	URL="https://api.telegram.org/bot${TOKEN}/sendMessage"		#api接口
-	curl -s -o /dev/null -X POST $URL -d chat_id=${chat_ID} -d parse_mode=${MODE} -d text="${message_text}" --max-time 10
+        TOKEN=${TG_TOKEN}       #TG机器人token
+        chat_ID=${TG_ID}                #用户ID或频道、群ID
+        message_text="${TG_MSG2}"               #要发送的信息
+        MODE="HTML"             #解析模式，可选HTML或Markdown
+        URL="https://api.telegram.org/bot${TOKEN}/sendMessage"          #api接口
+        curl -s -o /dev/null -X POST $URL -d chat_id=${chat_ID} -d parse_mode=${MODE} -d text="${message_text}" --max-time 10
 }
 
 function tg_push_message3() {
-	TOKEN=${TG_TOKEN}	#TG机器人token
-	chat_ID=${TG_ID}		#用户ID或频道、群ID
-	message_text="${TG_MSG3}"		#要发送的信息
-	MODE="HTML"		#解析模式，可选HTML或Markdown
-	URL="https://api.telegram.org/bot${TOKEN}/sendMessage"		#api接口
-	curl -s -o /dev/null -X POST $URL -d chat_id=${chat_ID} -d parse_mode=${MODE} -d text="${message_text}" --max-time 10
+        TOKEN=${TG_TOKEN}       #TG机器人token
+        chat_ID=${TG_ID}                #用户ID或频道、群ID
+        message_text="${TG_MSG3}"               #要发送的信息
+        MODE="HTML"             #解析模式，可选HTML或Markdown
+        URL="https://api.telegram.org/bot${TOKEN}/sendMessage"          #api接口
+        curl -s -o /dev/null -X POST $URL -d chat_id=${chat_ID} -d parse_mode=${MODE} -d text="${message_text}" --max-time 10
 }
 
 function tg_push_message4() {
-	TOKEN=${TG_TOKEN}	#TG机器人token
-	chat_ID=${TG_ID}		#用户ID或频道、群ID
-	message_text="${TG_MSG4}"		#要发送的信息
-	MODE="HTML"		#解析模式，可选HTML或Markdown
-	URL="https://api.telegram.org/bot${TOKEN}/sendMessage"		#api接口
-	curl -s -o /dev/null -X POST $URL -d chat_id=${chat_ID} -d parse_mode=${MODE} -d text="${message_text}" --max-time 10
+        TOKEN=${TG_TOKEN}       #TG机器人token
+        chat_ID=${TG_ID}                #用户ID或频道、群ID
+        message_text="${TG_MSG4}"               #要发送的信息
+        MODE="HTML"             #解析模式，可选HTML或Markdown
+        URL="https://api.telegram.org/bot${TOKEN}/sendMessage"          #api接口
+        curl -s -o /dev/null -X POST $URL -d chat_id=${chat_ID} -d parse_mode=${MODE} -d text="${message_text}" --max-time 10
 }
 
 function rclone_check() {
@@ -70,35 +91,35 @@ if [ ! -d ${log_dir} ]; then
 fi
 
 function qb_del() {
-	if [ ${leeching_mode} == "true" ]; then
-		if [ -e "${save_dir}" ]; then
-		    rm -rf ${save_dir}/${torrent_name}
-		    echo "[$(date '+%Y-%m-%d %H:%M:%S')] 删除成功！种子名称:${torrent_name}" >> ${log_dir}/qb.log
-		    TG_MSG3="[$(date '+%Y-%m-%d %H:%M:%S')] 删除成功！种子名称:${torrent_name}"
-		    tg_push_message3
-		else
-			rm -rf ${save_dir}/${torrent_name}
-			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 删除成功！种子文件:${torrent_name}" >> ${log_dir}/qb.log
-			TG_MSG3="[$(date '+%Y-%m-%d %H:%M:%S')] 删除成功！种子名称:${torrent_name}"
-			tg_push_message3
-		fi
-	else
-		echo "[$(date '+%Y-%m-%d %H:%M:%S')] 不自动删除已上传种子" >> ${log_dir}/qb.log
-		TG_MSG3="[$(date '+%Y-%m-%d %H:%M:%S')] 不自动删除已上传种子"
-		tg_push_message3
-	fi
+        if [ ${leeching_mode} == "true" ]; then
+                if [ -e "${save_dir}" ]; then
+                    rm -rf ${save_dir}/${torrent_name}
+                    echo "[$(date '+%Y-%m-%d %H:%M:%S')] 删除成功！种子名称:${torrent_name}" >> ${log_dir}/qb.log
+                    TG_MSG3="[$(date '+%Y-%m-%d %H:%M:%S')] 删除成功！种子名称:${torrent_name}"
+                    tg_push_message3
+                else
+                        rm -rf ${save_dir}/${torrent_name}
+                        echo "[$(date '+%Y-%m-%d %H:%M:%S')] 删除成功！种子文件:${torrent_name}" >> ${log_dir}/qb.log
+                        TG_MSG3="[$(date '+%Y-%m-%d %H:%M:%S')] 删除成功！种子名称:${torrent_name}"
+                        tg_push_message3
+                fi
+        else
+                echo "[$(date '+%Y-%m-%d %H:%M:%S')] 不自动删除已上传种子" >> ${log_dir}/qb.log
+                TG_MSG3="[$(date '+%Y-%m-%d %H:%M:%S')] 不自动删除已上传种子"
+                tg_push_message3
+        fi
 }
 
 function rclone_moveto() {
-	if [ "${type}" == "file" ]; then
-	    #find ${content_dir}/ -size -100000k -exec rm {} \;
-	    rclone_moveto_cmd=$(rclone -v moveto -P --transfers=${rclone_parallel} "${content_dir}" ${NAME}:${UP_PATH}"${content_dir}")
+        if [ "${type}" == "file" ]; then
+            #find ${content_dir}/ -size -100000k -exec rm {} \;
+            rclone_moveto_cmd=$(rclone -v moveto -P --transfers=${rclone_parallel} --tpslimit 1 --drive-upload-cutoff 1000T "${content_dir}" ${NAME}:${UP_PATH}"${content_dir}")
             TG_MSG1="[$(date '+%Y-%m-%d %H:%M:%S')] ${torrent_name} 上传到网盘 $NAME 的目录 ${UP_PATH}"${content_dir}" 已完成。"
-	elif [ "${type}" == "dir" ]; then
-	    #find ${content_dir}/ -size -100000k -exec rm {} \;
-	    rclone_moveto_cmd=$(rclone -v moveto -P --transfers=${rclone_parallel} "${content_dir}"/ ${NAME}:${UP_PATH}/"${torrent_name}")
+        elif [ "${type}" == "dir" ]; then
+            #find ${content_dir}/ -size -100000k -exec rm {} \;
+            rclone_moveto_cmd=$(rclone -v moveto -P --transfers=${rclone_parallel} --tpslimit 1 --drive-upload-cutoff 1000T "${content_dir}"/ ${NAME}:${UP_PATH}/"${torrent_name}")
             TG_MSG1="[$(date '+%Y-%m-%d %H:%M:%S')] ${torrent_name} 上传到网盘 $NAME 的目录 ${UP_PATH}/"${torrent_name}" 已完成。"
-	fi
+        fi
 }
 
 
