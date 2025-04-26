7a9f6418532ac5114c156703bd7c2f39d9f83de0
kissyouhunter <kissyouhunter@hotmail.com>
2025-04-27 00:50:46 +0800

    Update me-script.sh

--- VPS/me-script.sh
+++ VPS/me-script.sh
@@ -4,7 +4,34 @@ apt update
 sleep 1
 sudo timedatectl set-timezone Asia/Shanghai
 sleep 1
-echo 'net.core.default_qdisc=fq' | sudo tee -a /etc/sysctl.conf && echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.conf && sudo sysctl -p && sysctl net.ipv4.tcp_congestion_control
+cat > /etc/sysctl.conf << EOF
+fs.file-max = 6815744
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_rfc1337=0
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_moderate_rcvbuf=1
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
+net.ipv4.tcp_rmem=4096 87380 33554432
+net.ipv4.tcp_wmem=4096 16384 33554432
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+net.ipv6.conf.all.forwarding=1
+net.ipv6.conf.default.forwarding=1
+EOF
+sysctl -p && sysctl --system
 sleep 1
 curl -sSL https://get.docker.com | bash
 sleep 1
